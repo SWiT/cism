a38b4842888240f53163871980a4f507c821ccfc
Matthew Switlik <switlikm@gmail.com>
2021-02-15 10:01:49 -0500

    cleaner output for logging.

--- cism_cron.py
+++ cism_cron.py
@@ -26,16 +26,17 @@ for s in cfg['sensors']:
 DHT_SENSOR = Adafruit_DHT.AM2302   
 
 def on_publish(client,userdata,result):             #create function for callback
-    print("data published.")
+    #print("data published.")
     pass
 
 def on_disconnect(client, userdata, rc):
-   print("client disconnected ok")
+   print("MQTT client disconnected ok")
 
     
 mqttclient = paho.Client("connection01")               #create client object
 mqttclient.username_pw_set(MQTT_USER, MQTT_PASS)
 mqttclient.on_publish = on_publish                     #assign function to callback
+print("Connecting to MQTT:",MQTT_BROKER, MQTT_PORT)
 mqttclient.connect(MQTT_BROKER, MQTT_PORT)              #establish connection
 
 # Create the log file if empty.
@@ -43,33 +44,35 @@ f = open(LOGFILE, 'a+')
 if os.stat(LOGFILE).st_size == 0:
     f.write('DateTime,Name,Temperature(*'+DEGREE+'),Humidity(%)\r\n')
 
-print(time.strftime('%Y-%m-%d %H:%M:%S')+"\r\n")
+#print(time.strftime('%Y-%m-%d %H:%M:%S')+"\r\n")
     
 # Read each of the sensors data
 for sensor in SENSORS:
-    print("Reading '" + sensor.name + "'.")
+    #print("Reading '" + sensor.name + "'.")
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, sensor.gpiopin)
     
-    if DEGREE == "F":
-        temperature = temperature * (9 / 5) + 32
+    if humidity is not None and temperature is not None:        
+        if DEGREE == "F":
+            temperature = temperature * (9 / 5) + 32
 
-    # Add offsets
-    temperature += sensor.toffset
-    humidity    += sensor.hoffset
+        # Add offsets
+        temperature += sensor.toffset
+        humidity    += sensor.hoffset
 
-    print("Temperature {0:0.1f}.".format(temperature))
-    print("Humidity {0:0.1f}.".format(humidity))
-
-    if humidity is not None and temperature is not None:
+        #print("T: {0:0.1f} H: {1:0.1f}".format(temperature, humidity))
+    
         # Publish the data
         ret = mqttclient.publish(sensor.name+"/temperature","{0:0.1f}".format(temperature))
         ret = mqttclient.publish(sensor.name+"/humidity","{0:0.1f}".format(humidity))
         
-        print("Logging to file.\r\n")
-        f.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sensor.name, temperature, humidity))
+        #print("Logging to file.\r\n")
+        output = '{0}, {1}, {2:0.1f}, {3:0.1f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sensor.name, temperature, humidity)
+        f.write(output+"\n")
+        print(output)
     else:
-        ret = mqttclient.publish(sensor.name,"FAILED")
-        print("Failed to retrieve data from sensor")
+        print("Failed to retrieve data from", sensor.name)
+        #print("Logging to file.\r\n")
+        f.write('{0}, {1}, failed, failed\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sensor.name))
     
    
 mqttclient.on_disconnect = on_disconnect
